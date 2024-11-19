774b83f1b2bfabc9538bf592ca98986d362e6366
youjian <1124895509@qq.com>
2024-11-19 19:10:58 +0800

    Add clang-format.

--- include/simple_chassis_controller/simple_chassis_controller.h
+++ include/simple_chassis_controller/simple_chassis_controller.h
@@ -10,21 +10,23 @@
 
 namespace simple_chassis_controller {
 
-class SimpleChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
- public:
+class SimpleChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface>
+{
+public:
   SimpleChassisController() = default;
   ~SimpleChassisController() override = default;
 
-  bool init(hardware_interface::EffortJointInterface *effort_joint_interface,
-            ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
+  bool init(hardware_interface::EffortJointInterface* effort_joint_interface, ros::NodeHandle& root_nh,
+            ros::NodeHandle& controller_nh) override;
 
-  void update(const ros::Time &time, const ros::Duration &period) override;
+  void update(const ros::Time& time, const ros::Duration& period) override;
 
   hardware_interface::JointHandle front_left_joint_, front_right_joint_, back_left_joint_, back_right_joint_;
- private:
+
+private:
   int state_{};
   ros::Time last_change_;
 };
-}// namespace simple_chassis_controller
+}  //  namespace simple_chassis_controller
 
-#endif //SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
+#endif  // SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
--- src/simple_chassis_controller.cpp
+++ src/simple_chassis_controller.cpp
@@ -6,31 +6,31 @@
 #include <pluginlib/class_list_macros.hpp>
 
 namespace simple_chassis_controller {
-bool SimpleChassisController::init(hardware_interface::EffortJointInterface *effort_joint_interface,
-                                   ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) {
-  front_left_joint_ =
-      effort_joint_interface->getHandle("left_front_wheel_joint");
-  front_right_joint_ =
-      effort_joint_interface->getHandle("right_front_wheel_joint");
+bool SimpleChassisController::init(hardware_interface::EffortJointInterface* effort_joint_interface,
+                                   ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh)
+{
+  front_left_joint_ = effort_joint_interface->getHandle("left_front_wheel_joint");
+  front_right_joint_ = effort_joint_interface->getHandle("right_front_wheel_joint");
   back_left_joint_ = effort_joint_interface->getHandle("left_back_wheel_joint");
-  back_right_joint_ =
-      effort_joint_interface->getHandle("right_back_wheel_joint");
+  back_right_joint_ = effort_joint_interface->getHandle("right_back_wheel_joint");
 
   return true;
 }
 
-void SimpleChassisController::update(const ros::Time &time, const ros::Duration &period) {
+void SimpleChassisController::update(const ros::Time& time, const ros::Duration& period)
+{
   double tau = 0.2;  // torque
-  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
-  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
-  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
-  static double cmd_[6][4] = {{tau, tau, tau, tau}, //  forward
-                              {-2 * tau, -2 * tau, -2 * tau, -2 * tau}, //  backward
-                              {-tau, tau, tau, -tau}, //  left
-                              {2 * tau, -2 * tau, -2 * tau, 2 * tau}, //  right
-                              {2 * tau, -2 * tau, 2 * tau, -2 * tau}, //  clockwise
-                              {-tau, tau, -tau, tau}};  //  counterclockwise
-  if ((time - last_change_).toSec() > 2) {
+                     // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
+                     // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
+                     // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
+  static double cmd_[6][4] = { { tau, tau, tau, tau },                      //  forward
+                               { -2 * tau, -2 * tau, -2 * tau, -2 * tau },  //  backward
+                               { -tau, tau, tau, -tau },                    //  left
+                               { 2 * tau, -2 * tau, -2 * tau, 2 * tau },    //  right
+                               { 2 * tau, -2 * tau, 2 * tau, -2 * tau },    //  clockwise
+                               { -tau, tau, -tau, tau } };                  //  counterclockwise
+  if ((time - last_change_).toSec() > 2)
+  {
     state_ = (state_ + 1) % 6;
     last_change_ = time;
   }
@@ -41,4 +41,4 @@ void SimpleChassisController::update(const ros::Time &time, const ros::Duration 
 }
 
 PLUGINLIB_EXPORT_CLASS(simple_chassis_controller::SimpleChassisController, controller_interface::ControllerBase)
-}
+}  // namespace simple_chassis_controller
